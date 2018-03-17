e8751e17637849682f6dba7ebe9791f20fb4ba20
Aarif Sumra <aarif.sumra@gmail.com>
2018-03-17 15:14:20 +0900

    Issue #7 fixed added test to check if posterimage download is cancelled as soon as prepare for reuse is called

--- eigami/View/MovieListCell.swift
+++ eigami/View/MovieListCell.swift
@@ -13,6 +13,8 @@ class MovieListCell: UICollectionViewCell {
     static let identifier = "MovieListCellIdentifier"
     @IBOutlet weak var posterImageView: UIImageView!
     
+//    private task
+    
     // MARK: Life cycle
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -26,7 +28,6 @@ class MovieListCell: UICollectionViewCell {
     func configure(forItem item: Movie) {
         if let path = item.posterPath, let posterUrl = URL(string: "https://image.tmdb.org/t/p/w500" + path) {
             posterImageView.kf.indicatorType = .activity
-            posterImageView.kf.setImage(with: posterUrl)
             posterImageView.kf.setImage(with: posterUrl, placeholder: #imageLiteral(resourceName: "No_image_poster"), completionHandler: {
                 (image, error, cacheType, imageUrl) in
                 if image == nil {
