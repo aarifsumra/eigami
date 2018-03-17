a89b39106e6d13aeb8889f23156f195d64e04c3a
Aarif Sumra <aarif.sumra@gmail.com>
2018-03-17 20:45:17 +0900

    added spec and actual files

---
+++ eigami/View/PopularTabViewController.swift
@@ -0,0 +1,12 @@
+//
+//  PopularTabViewController.swift
+//  eigami
+//
+//  Created by Aarif Sumra on 2018/03/17.
+//
+
+import UIKit
+
+class PopularTabViewController: UIViewController {
+    
+}
---
+++ eigamiTests/View/PopularTabViewControllerSpec.swift
@@ -0,0 +1,32 @@
+//
+//  PopularTabViewControllerSpec.swift
+//  eigamiTests
+//
+//  Created by Aarif Sumra on 2018/03/17.
+//
+
+import Quick
+import Nimble
+import Moya
+import RxSwift
+import RxTest
+import RxCocoa
+
+@testable import eigami
+
+class PopularTabViewControllerSpec: QuickSpec {
+    override func spec() {
+        describe("Popular Tab") {
+            var sut: PopularTabViewController!
+            
+            beforeEach {
+                sut = 
+            }
+            
+            
+            it("has a title name popular") {
+                
+            }
+        }
+    }
+}
